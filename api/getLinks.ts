027a4e6cb482cdc894435d3000822743e0f9d122
Yash Singh <saiansh2525@gmail.com>
2024-03-06 17:26:20 -0800

    fix: use node runtime

--- api/getLinks.ts
+++ api/getLinks.ts
@@ -2,6 +2,8 @@ import Redis from "ioredis";
 
 const client = new Redis(process.env.REDIS_URL);
 
+export const runtime = "node";
+
 export async function GET() {
-  return { data: await client.get("ngrok_link") };
+  return new Response(JSON.stringify({ data: await client.get("ngrok_link") }));
 }
